4e1d9b9e63e13d311c4da4d6be7ae4d352374894
Manow <121009820+Nutcha-Kaew@users.noreply.github.com>
2025-03-03 08:58:27 +0000

    mock data

---
+++ src/transaction/transaction.mock.ts
@@ -0,0 +1,19 @@
+export const transactions = [
+    {
+      id: 1,
+      userId: 1,
+      cryptoId: 1,
+      amount: 0.5,
+      price: 60000,
+      timestamp: new Date(),
+    },
+    {
+      id: 2,
+      userId: 2,
+      cryptoId: 2,
+      amount: 2,
+      price: 4000,
+      timestamp: new Date(),
+    },
+  ];
+  
---
+++ src/user/user.mock.ts
@@ -0,0 +1,5 @@
+export const users = [
+    { id: 1, username: 'manow_nutcha', email: 'nutchakk.dev@gmail.com' },
+    { id: 2, username: 'nut_kaew', email: 'nathchakarwcanthr7@gmail.com' },
+  ];
+  
