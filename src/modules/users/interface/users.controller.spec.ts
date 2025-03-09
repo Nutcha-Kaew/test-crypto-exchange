8898139190d7c6db14439b6b29a5d9ed761ec4e5
Manow <121009820+Nutcha-Kaew@users.noreply.github.com>
2025-03-09 13:28:29 +0000

    fix: pkg

--- src/modules/users/interface/users.controller.spec.ts
+++ src/modules/users/interface/users.controller.spec.ts
@@ -12,6 +12,7 @@ describe('UsersController', () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [
+
         {
           provide: UsersService,
           useValue: {
@@ -26,7 +27,7 @@ describe('UsersController', () => {
   });
 
   it('should return user with wallets', async () => {
-    const mockUser: User = { user_id: '1', username: 'manow_nutcha', email: 'nutchakk.dev@gmail.com', password_hash: 'p@assw0rd' };
+    const mockUser: User = { user_id: '1', username: 'manow_nutcha', email: 'nutchakk.dev@gmail.com',, password_hash: 'p@assw0rd' };
     jest.spyOn(service, 'findUserWithWallets').mockResolvedValue(mockUser);
 
     const result = await controller.getUserWallets('1');
