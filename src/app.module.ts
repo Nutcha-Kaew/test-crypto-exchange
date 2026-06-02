a4416ee034af2682d5276c4ea492d2e943d79ac5
Nutcha Kaewjan <nutchamanow@Nutchas-MacBook-Air.local>
2026-06-02 19:23:11 +0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
 import { CryptocurrencyModule } from './modules/cryptocurrencies/cryptocurrency.module';
@@ -6,14 +7,22 @@ import { WalletsModule } from './modules/wallets/wallets.module';
 import { User } from './modules/users/domain/user.entity';
 import { Cryptocurrency } from './modules/cryptocurrencies/domain/cryptocurrency.entity';
 import { Wallets } from './modules/wallets/domain/wallet.entity';
+import { Transaction } from './modules/transaction/domain/transaction.entity';
+import { Order } from './modules/order/domain/order.entity';
+import { FiatTransaction } from './modules/fiat-transaction/domain/fiat-transaction.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
-      entities: [User, Cryptocurrency, Wallets],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'sqlite',
+        database: config.get<string>('DATABASE_FILE', 'database.sqlite'),
+        entities: [User, Cryptocurrency, Wallets, Transaction, Order, FiatTransaction],
+        synchronize: true,
+      }),
     }),
     UsersModule,
     CryptocurrencyModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SeederService } from './infrastructure/database/seeder.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const seeder = app.get(SeederService);
-  await seeder.seed();
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
